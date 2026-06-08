48786ddb488c5c8f7015e8ec6aab88271ed771cf
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-09 05:14:16 +0700

    Replace Files

---
+++ post-fs-data.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+
+if [ -f "$MODDIR/core/yay_post.sh" ]; then
+    sh "$MODDIR/core/yay_post.sh"
+fi
+
+exit 0
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# remove configs
+rm -rf /data/adb/sys_YAY
+
+# clean our symlinks
+rm /data/adb/ap/bin/netswitch
+rm /data/adb/ksu/bin/netswitch
