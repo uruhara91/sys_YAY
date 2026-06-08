f4b8bf9b908b6dbc1a2b11ccb07cb34092b46a4d
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-09 06:37:24 +0700

    Update Netswitch

--- customize.sh
+++ customize.sh
@@ -1,18 +1,22 @@
 ISOLATED="/data/adb/.config/sys_YAY/isolated.json"
-if [ ! -f "$ISOLATED" ]; then
-  mkdir -p "$(dirname "$ISOLATED")"
-  touch "$ISOLATED"
+if [ ! -f $ISOLATED ]; then
+	mkdir -p $(dirname $ISOLATED)
+	touch $ISOLATED
 fi
 
 if [ "$KSU" = "true" ] || [ "$APATCH" = "true" ]; then
-  rm -f "$MODPATH/action.sh"
-  touch "$MODPATH/skip_mount"
-  for dir in "/data/adb/ap/bin" "/data/adb/ksu/bin"; do
-    if [ -d "$dir" ]; then
-      ln -sf "/data/adb/modules/sys_YAY/system/bin/netswitch" "$dir/netswitch"
-    fi
-  done
+	# remove action on APatch / KernelSU
+	rm "$MODPATH/action.sh"
+	# skip mount on APatch / KernelSU
+	touch "$MODPATH/skip_mount"
+	# symlink ourselves on $PATH
+	manager_paths="/data/adb/ap/bin /data/adb/ksu/bin"
+	for dir in $manager_paths; do
+		if [ -d "$dir" ]; then
+			echo "- creating symlink in $dir"
+			ln -sf /data/adb/modules/sys_YAY/system/bin/netswitch "$dir/netswitch"
+		fi
+	done
 fi
 
-set_perm_recursive "$MODPATH/core" 0 0 0755 0755
 set_perm_recursive "$MODPATH/system" 0 0 0755 0755
