b0d2da4b5d559d4ea7f1a2e28a0e7f7107eebc91
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-18 21:45:06 +0700

    Configure game modes and downscaling for overlays

--- core/yay_game.sh
+++ core/yay_game.sh
@@ -45,4 +45,19 @@
 
 echo "Game Log Napped." >> "$LOG_FILE"
 
+# 1. Downscaler
+device_config put game_overlay com.garena.game.df mode=2,downscaleFactor=0.7
+device_config put game_overlay com.mobile.legends mode=2,downscaleFactor=0.7
+device_config put game_overlay com.voodoo.almanac mode=3,downscaleFactor=0.7
+
+# 2. Game Mode
+cmd game mode performance com.garena.game.df
+cmd game mode performance com.mobile.legends
+cmd game mode battery com.voodoo.almanac
+
+# 3. Lock
+device_config set_sync_disabled_for_tests persistent
+
+echo "Game Mode Configured." >> "$LOG_FILE"
+
 exit 0
