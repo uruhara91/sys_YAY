d7408568b8dc613fe89c86d8d9b4b571eda60117
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-20 04:40:13 +0700

    Add compile-time Parcel layout invariants

--- zygisk/jni/binder.hpp
+++ zygisk/jni/binder.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <jni.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <sys/types.h>
 
@@ -15,6 +16,13 @@ struct PParcel {
     char* data;
     size_t data_size;
 };
+
+static_assert(offsetof(PParcel, data) == sizeof(size_t),
+              "Unexpected PParcel::data offset");
+static_assert(offsetof(PParcel, data_size) == 2 * sizeof(size_t),
+              "Unexpected PParcel::data_size offset");
+static_assert(sizeof(PParcel) == 3 * sizeof(size_t),
+              "Unexpected PParcel prefix size");
 
 bool getMapping(const char* library_name, ino_t* inode, dev_t* device);
 uint32_t getStaticIntFieldJni(JNIEnv* env, const char* class_name, const char* field_name);
